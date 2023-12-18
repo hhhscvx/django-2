4dd4f06266866f52e884ea1364f08de281e4c31b
hhhscvx <hhhscvx@yandex.ru>
2023-12-18 14:55:22 +0500

    добавлены регистрация и модель Profile

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,  #
+                                on_delete=models.CASCADE)  # Удаляем Profile при удалении user
+    date_of_birth = models.DateTimeField(blank=True, null=True)  # Может быть пустым и иметь значение null
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/',
+                              # путь, в котором будут сохранены загруженные изображения (аватарки); (users/2023/12/18)
+                              blank=True)
+
+    def __str__(self):
+        return f'Профиль пользователя - {self.user.username}'
