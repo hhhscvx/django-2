542eca33ac27c0edc3f899a96358c70a9e894e16
hhhscvx <hhhscvx@yandex.ru>
2023-12-19 19:08:15 +0500

    добавлена авторизация через гугл и вк

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#-ny34-6%wb#t)prr$i8!g0-6+=n0jt+z5b24zlw8evb7_anvz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']  # по всем этим ссылкам можно заходить на проект
 
 # Application definition
 
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -135,4 +137,25 @@ TIME_ZONE = 'Asia/Yekaterinburg'
 AUTHENTICATION_BACKENDS = [  # Проверяет по каждому варианту аутентификации, чтобы хотя бы один подошел
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.vk.VKOAuth2'
 ]
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '834417827638-mdqn1avvlb37b88ivmrnep0u7gbtjrvi.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_VK_OAUTH2_KEY = '51817971'
+SOCIAL_AUTH_VK_OAUTH2_SECRET = 'w3MQl7KDwjOE95Vlrkf4'
