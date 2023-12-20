56f5510f89731126635ab45f64e88b4cb500e9a5
hhhscvx <hhhscvx@yandex.ru>
2023-12-20 08:12:22 +0500

    model for images created + admin site add

---
+++ bookmarks/images/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'image', 'created']
+    list_filter = ['created']
---
+++ bookmarks/images/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+
+
+class Image(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,  # Указывает на auth на данный момент пользователя
+                             related_name='images_created',  # по этому имени обращаемся к связанным images с user`ом
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=24)
+    slug = models.SlugField(max_length=24,
+                            blank=True)
+    image = models.ImageField(upload_to='%Y/%m/%d')
+    url = models.URLField()
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)  # auto_now_add - устанавлеваем now время и дату при создании
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,  # Многие ко многим понравившееся image и user`ы
+                                       related_name='images_liked',
+                                       blank=True)
+
+    class Meta:
+        indexes = [
+            # короче походу это надо так как чаще обращаемся к более свежим image`ам, будет сортировано сразу по created
+            models.Index(fields=['-created'])
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)  # делаем slug по title`у
+        return super().save(*args, **kwargs)
